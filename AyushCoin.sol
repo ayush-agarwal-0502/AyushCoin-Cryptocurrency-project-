e6a6ab3230c923cdaafad6f668972aa9d020047e
Ayush Agarwal <86561124+ayush-agarwal-0502@users.noreply.github.com>
2023-04-21 20:08:33 +0530

    Update AyushCoin.sol

--- AyushCoin.sol
+++ AyushCoin.sol
@@ -1,13 +1,16 @@
 // SPDX-License-Identifier: MIT
+// License information
+
+// solidity version above 0.8.0
 pragma solidity ^0.8.0;
+// Importing standard contract from OZ
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
 
-// replace the name of your currency with LW3Token. Do not give spaces in the name.
-// replacing the name of the contract will not affect the name of the currency
-// but for uniformity it is recommended
-
+// contract 
 contract AyushCoin is ERC20 {
+    // constructor function for contract
     constructor(string memory _name, string memory _symbol) ERC20(_name,_symbol) {
+        // the smart contract deployer receives 10 AYC (AyushCoins) 
         _mint(msg.sender, 10 * 10 ** 18);
     }
 }
